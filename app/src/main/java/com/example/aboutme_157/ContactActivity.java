2676b8d9c83777f27c12074115c1098a3d55dbf4
Cristian Vidal <Cristian.vidal.lopez@gmail.com>
2020-12-23 13:44:36 -0300

    ADD: añadimos la logica de intent y las vistas de contacto y fragmento.

--- app/src/main/java/com/example/aboutme_157/ContactActivity.java
+++ app/src/main/java/com/example/aboutme_157/ContactActivity.java
@@ -2,13 +2,54 @@ package com.example.aboutme_157;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.aboutme_157.databinding.ActivityContactBinding;
 
 public class ContactActivity extends AppCompatActivity {
+
+    private ActivityContactBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        mBinding = ActivityContactBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.imageViewLinkeding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent linkedingIntent = new Intent(Intent.ACTION_VIEW);
+                linkedingIntent
+                        .setData(Uri.parse("https://www.linkedin.com/in/cristianvidallopez/"));
+                startActivity(linkedingIntent);
+
+                Toast.makeText(ContactActivity.this, "Si pasa", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        mBinding.imageViewPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareWithWsp("Hola esto va para el whassapp");
+            }
+        });
+
+
     }
+
+    public void shareWithWsp(String message){
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+        sendIntent.setType("text/plain");
+       // sendIntent.setPackage("com.whatsapp");
+        startActivity(sendIntent);
+    }
+
+
 }
--- app/src/main/java/com/example/aboutme_157/MainActivity.java
+++ app/src/main/java/com/example/aboutme_157/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.aboutme_157;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -12,6 +14,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +28,20 @@ public class MainActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
+        //TODO inicializar viewBinding y aplicarlo
+        ImageView imageView = findViewById(R.id.imageViewMain);
+        Glide.with(this)
+                .load("https://lh3.googleusercontent.com/3oR75ba4xCtdCbSDyxBbTRYtwptE9pPksLDcc2HLHGYWY2P-nM1MOQe4NhuOpwSqWdNWeuE_JzyIdwAh6UXd8mdZQTLVFUt5E6r12q6x_cp9Y8FeX37iRmtJhUac6-Yqj3VHEsC4jGJ30I87plkX1ZNmqvArkjr9SJKz6ju7RfeVgRS1tkCgiEl7lc3qivf5ZoHEa26p_wTP6PvVk08Iuj9GQoqtjBTnAKQKpFK4_HPDStzb3YAgoRRR55ElmR-RUflfFp7haVXJe3kDPeFPuelqOh_Hu1vy8IdNmPc0YWKV7QxmgNbpz-N9zIJNEkjrGxnnOMsfpS5oXjq2FRAXMp2-p1J_ewBsZog_FKwye_WTj9ObGdnALPKykpqm4DlAJhCtkcrU2PkavYphAEYSk7KXFGXI1DSo3wKtxke12jT34wUG9LPhsaLZkHoo5Qke8ohCVIEPpHwtKiJf0GmzXHfjgxQXbrH90o3wY3HgJ1MepoX7beHE5MM2nhR4QRreSMn4_m_mz4deZ9MRbZv3BsmesqcLs_uCiVO0XzTLVE7qY44XbePHaBHW8FeZNWjuP16eg3gDtHWb0UuggleOKe9IeNK_IMdCCINzZgM_mtJkzq04dShWeNw2ZZ594MFTEhBzumvjEb1Wh60zZvyqMl2ktresYz-KGvQJjAEqw2JUq5UnBRo5HUT3duEcjA=w755-h1006-no?authuser=0")
+                .centerCrop()
+                .into(imageView);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent mIntent = new Intent(MainActivity.this, ContactActivity.class);
+                startActivity(mIntent);
             }
         });
     }
